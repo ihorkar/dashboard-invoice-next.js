6d1ccd0f0a50b807ccd833dc93156ec113430c8f
ihorkar <ihorkarpiuk0709@gmail.com>
2024-03-06 23:24:47 +0200

    feat: Handle 404 errors with notFund func

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,8 +63,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  // throw new Error('Failed to delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
